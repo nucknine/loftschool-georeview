139e001396073183f0c536f8ce168e25e93438a9
nucknine <nucknine@gmail.com>
2018-06-07 17:22:48 +0300

    todo: claster balloon

--- source/js/app.js
+++ source/js/app.js
@@ -1,22 +1,31 @@
 /* globals svg4everybody, ymaps */
 
-(function() {
-    'use strict';
+    svg4everybody();
 
-    svg4everybody();    
+    closeBalloon();
 
     function getAddress(coords) {
         return new Promise((resolve) => {
             ymaps.geocode(coords).then(function (res) {
                 var firstGeoObject = res.geoObjects.get(0);
-                var address =  firstGeoObject.getAddressLine();
+                var address = firstGeoObject.getAddressLine();
+
                 return resolve(address);
             });
         });
     }
 
+    function closeBalloon () {
+        const form = document.querySelector('#form');
+
+        form.style.display = 'none';
+    }
+
     var reviews = {
-        items: []
+        items: [],
+        clickAddress: {
+            address: ''
+        }
     };
 
     function createReview (form, coords) {
@@ -29,8 +38,19 @@
             review.address = result;
         });
         review.comment = form.querySelector('#comment').value;
+        review.date = new Date().toLocaleString();
+        console.log(reviews);
+        if (review.name && review.spot && review.comment) {
+            reviews.items.push(review);            
+            form.querySelector('#firstName').value = null;
+            form.querySelector('#spot').value = null;
+            form.querySelector('#comment').value = null;
 
-        reviews.items.push(review);
+            return true;
+        }
+
+        return false;
+
     }
 
     let clusterer;
@@ -43,67 +63,99 @@
         }, {
             searchControlProvider: 'yandex#search'
         });
-        
+
         /**
          * click по карте
          */
-
         myMap.events.add('click', function (e) {
-            // console.log(e.get('target'));
+            var coords = e.get('coords');
 
-            if (!myMap.balloon.isOpen()) {
-                var coords = e.get('coords');
+            getAddress(coords).then((result)=>{
+                reviews.clickAddress.address = result;
+            });
+            
+            console.log(reviews.clickAddress.address);
 
-                var p = new Promise(async function(resolve) {
-                    var bl = await myMap.balloon.open(coords, {
-                        content: '<div id="form" class="baloon-body"><div>'
-                    });
-                    var form;
-                    
-                    // setTimeout(() => {
-                        form = document.querySelector('#form');
-                        console.log(bl);
-                        resolve(form);                        
-                    // }, 500);
+            let form = document.querySelector('#form');
 
-                });
+            if (form.style.display !== 'block') {
 
-                p.then(function(form) {
-                    renderForm(form);
+                var position = e.get('position');
 
-                    document.querySelector('#button-add').addEventListener('click', () => {
-
-                        createReview(form, coords);
-                        createPlaceMark(coords);
-
-                        console.log(reviews);
-                    });
-                });
+                openBalloon(position, form);
+                renderForm(form, coords);
+                renderFeed(form, coords);
 
             } else {
-                myMap.balloon.close();
+                closeBalloon();
             }
-
         });
 
-        function renderForm (balloon) {
+        function openBalloon ([left, top], form) {
+
+            form.style.display = 'block';
+            form.style.left = left + 'px';
+
+            let toTopEdge = window.innerHeight - top;
+            let toLeftEdge = window.innerWidth - left;
+
+            form.style.top = toTopEdge > form.offsetHeight ? top + 'px' : top - form.offsetHeight + 'px';
+            form.style.left = toLeftEdge > form.offsetWidth ? left + 'px' : left - form.offsetWidth + 'px';
+
+        }
+
+        function renderFeed(form, [x, y]) {
+            let feed = form.querySelector('#feed');
+            const template = document.querySelector('#feed-template').textContent;
+            const comments = getReviews(x, y);
+            const render = Handlebars.compile(template);
+            const html = render(comments);
+
+            feed.innerHTML = html;
+        }
+
+        function renderForm (form, coords) {
+
             let template;
 
             template = document.querySelector('#template').textContent;
 
             const render = Handlebars.compile(template);
-            const html = render(reviews);
 
-            balloon.innerHTML = html;
+            const html = render(reviews.clickAddress);
+
+            form.innerHTML = html;
+
+            document.querySelector('#button-add').addEventListener('click', () => {
+                if (createReview(form, coords)) {
+                    createPlaceMark(coords);
+                    renderFeed(form, coords);
+                } else {
+                    return false;
+                }
+            });
+        }
+
+        function getReviews (x, y) {
+            let feed = {
+                items: []
+            };
+
+            for (let item of reviews.items) {
+                let [coordX, coordY] = item.coords;
+
+                if (x == coordX && y == coordY) {
+                    feed.items.push(item);
+                }
+            }
+
+            return feed;
         }
 
         function createPlaceMark (coords) {
             let myPlacemark = new ymaps.Placemark(coords, {
-                hintContent: 'Собственный значок метки',
-                balloonContent: [
-                    '<div id="result" class="baloon-body">',
-                    '</div>'
-                ].join('')
+                hintContent: 'Место с отзывом',
+                balloonContent: '',
             }, {
                 iconLayout: 'default#image',
                 iconImageHref: 'assets/img/icons/mark-gray.png',
@@ -111,7 +163,14 @@
             });
 
             myPlacemark.events.add('click', function (e) {
-                console.log(e);
+                var coords = myPlacemark.geometry.getCoordinates();
+
+                var position = e.get('position');
+                let form = document.querySelector('#form');
+
+                openBalloon(position, form);
+                renderForm(form, coords);
+                renderFeed(form, coords);
             });
             clusterer.add(myPlacemark);
         }
@@ -126,5 +185,3 @@
         .add(clusterer);
     }) // инициализация карты
     .catch(e => alert('Ошибка: ' + e.message));
-
-})();
